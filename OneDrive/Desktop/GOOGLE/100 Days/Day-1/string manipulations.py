ecce992aa07516d93cfcd516a209b13f869ed945
Ganesh <90366673+ganeshpottipadu@users.noreply.github.com>
2025-09-26 10:17:42 +0530

    string manipulations.py

--- OneDrive/Desktop/GOOGLE/100 Days/Day-1/string manipulations.py
+++ OneDrive/Desktop/GOOGLE/100 Days/Day-1/string manipulations.py
@@ -1,6 +1,44 @@
-print("Welcome to the Brand Name Generator")
-A=input("what's the name of the city you grew up in?\n")
-B=input("what's your pet's name?\n")
-print("your band name could be ", A+B)
+# Joining two strings with + 
+first_name = "Ganesh"
+last_name = "Pottipadu"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+# Finding the lenght of string
+text = "Python Programming"
+print("Length of text:", len(text))
+
+# changing the case 
+msg = "hello python learners"
+print("Uppercase:", msg.upper())
+print("Lowercase:", msg.lower())
+print("Title Case:", msg.title())
+
+# Indexing & Slicing 
+word = "Python"
+print("First character:", word[0])    # P
+print("Last character:", word[-1])    # n
+print("First three letters:", word[:3])  # Pyt
+print("Last three letters:", word[-3:])  # hon
+
+
+# Splitting & Joining
+fruits = "apple,banana,orange"
+fruit_list = fruits.split(",")
+print("After split:", fruit_list)
+joined = " - ".join(fruit_list)
+print("After join:", joined)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
