d182a4b04d6f73af49437a67753fc475b71b698f
ganeshpottipadu <ganeshpottipadu@gmail.com>
2025-09-26 10:06:42 +0530

    added Day-1 Python files

---
+++ OneDrive/Desktop/GOOGLE/100 Days/Day-1/string manipulations.py
@@ -0,0 +1,6 @@
+print("Welcome to the Brand Name Generator")
+A=input("what's the name of the city you grew up in?\n")
+B=input("what's your pet's name?\n")
+print("your band name could be ", A+B)
+
+
